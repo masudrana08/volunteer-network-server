508ee98ec9b43cb1cd342a7c0e684afe58bc829d
masudrana08 <rana028511@gmail.com>
2020-10-04 10:13:04 +0600

    event cancel functionality done

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
-
-require('dotenv').config()
+const ObjectID = require('mongodb').ObjectID
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
+
+require('dotenv').config()
 
 
 // var admin = require("firebase-admin");
@@ -19,16 +20,15 @@ app.use(bodyParser.urlencoded({extended:false}))
 // });
 
 
-
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fjsvr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.fjsvr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const eventsCollection = client.db(`${process.env.DB_NAME}`).collection("events");
-  const registeredEvents = client.db(`${process.env.DB_NAME}`).collection("registered");
+  const eventsCollection = client.db(process.config.DB_NAME).collection("events");
+  const registeredEvents = client.db(process.config.DB_NAME).collection("registered");
   //mongo scope start
-
   app.get('/show-volunteers', (req,res)=>{
+   
     eventsCollection.find({})
     .toArray((error, documents)=>{
       res.send(documents)
@@ -38,7 +38,7 @@ client.connect(err => {
     app.post('/submit-form',(req,res)=>{
       registeredEvents.insertOne(req.body)
       .then(result=>{
-        console.log(result)
+        
         res.send(result.insertedCount>0)
       })
     })
@@ -50,6 +50,16 @@ client.connect(err => {
         
       })
     })
+
+    app.delete('/cancel-event',(req,res)=>{
+      console.log(req.headers.id)
+      registeredEvents.deleteOne({_id:ObjectID(req.headers.id)})
+      .then(result=>{
+        
+        res.send(result.deletedCount>0)
+      })
+    })
+
   //mongo scope end
 });
 
